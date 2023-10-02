cc7d1fabe054d097f3dd5f9fc144e9a211b93a24
Ian Zhang <benchi2@pdx.edu>
2023-10-01 22:53:27 -0500

    the decisionTreeExperiment class

--- project2.py
+++ project2.py
@@ -4,11 +4,13 @@ import os
 import io
 import glob
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, f1_score
 
 class dataExtractor:
     def __init__(self, zipPath):
-        self.allData = None
-        self._initializeAllData()
+        self.allData = self._initializeAllData()
         self.zipPath = os.path.join("./", zipPath)
         self.folderName = None
         self._extractFile()
@@ -21,9 +23,8 @@ class dataExtractor:
             for example in [100, 1000, 5000]:
                 clauseDict[example] = clauseDict.get(example, {"train": None, "valid": None, "test": None})
             result[clause] = result.get(clause, clauseDict)
-        self.allData = result
+        return result
     def _extractFile(self):
-        counter = 0
         with zipfile.ZipFile(self.zipPath, "r") as folder:
             csvList = [_ for _ in folder.namelist() if _.endswith(".csv")]
             for oneCsv in csvList:
@@ -34,7 +35,20 @@ class dataExtractor:
                 with folder.open(oneCsv) as f:
                     dataSet = np.genfromtxt(f, delimiter=",", dtype=int)
                     self.allData[clauseNumber][exampleNumber][dataType] = dataSet
-                    counter += 1
-        print(counter)
 
-ext = dataExtractor("project2_data.zip")
+
+class decisionTreeExperiment(dataExtractor):
+    def __init__(self, zipPath):
+        super.__init__(self, zipPath)
+        self.bestParams = self._initializeAllData()
+        self._initializeBestParams()
+    
+    def _initializeBestParams(self):
+        for key1, val1 in self.bestParams.items():
+            for key2, val2 in val1.items():
+                val2 = {"bestParams": None, "accuracy": None, "F1Score": None}
+        
+
+
+
+# ext = dataExtractor("project2_data.zip")
