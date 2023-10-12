174964e600eaf2c04a9e8dca6a983d635b459268
Ian Zhang <benchi2@pdx.edu>
2023-10-12 15:07:42 -0500

    mist experiment

--- project2.py
+++ project2.py
@@ -14,6 +14,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_openml
 class dataExtractor:
     def __init__(self, zipPath):
@@ -219,6 +220,114 @@ def mnistExperiment(identifier, aClassifier):
     print(f"Model Identifier: {identifier}\n")
     print(f"Accuracy on MNIST: {accuracy}\n")
     
+class mnistExperiment:
+    def __init__(self):
+        X, y = fetch_openml("mnist_784", version=1, return_X_y=True, parser="auto")
+        X = X / 255.
+        # rescale the data, use the traditional train/test split
+        # (60K: Train) and (10K: Test)
+        X_train, X_test = X[:60000], X[60000:]
+        y_train, y_test = y[:60000], y[60000:]
+        self.trainX = X_train
+        self.trainY = y_train
+        self.testX = X_test
+        self.testY = y_test
+        _, self.validationDataX, _y, self.validationDataY = train_test_split(self.trainX, self.trainY, test_size=0.1, stratify=y_train, random_state=4)
+        self.scoring = {
+            'f1_score': make_scorer(f1_score, zero_division=0.0, average="binary"),
+            'accuracy': make_scorer(accuracy_score),
+            'precision': make_scorer(precision_score, zero_division=0.0, average="binary"),
+            'recall': make_scorer(recall_score, average="binary")
+        }
+        self.bastParams = None
+        self.validationAccuracy = None
+        self.testAccuracy = None
+    def _randomizedGridSearch(self, model, paramGrid):
+        model = model
+        search = RandomizedSearchCV(estimator=model, param_distributions=paramGrid, n_iter=50, scoring=self.scoring, n_jobs=-2, refit="f1_score")
+        search.fit(self.validationDataX, self.validationDataY)
+        return search.best_params_, search.best_estimator_
+
+    def search(self, paramGrid, identifier):
+        searchModel = None
+        if identifier == "D":
+            searchModel = DecisionTreeClassifier()
+        elif identifier == "BG":
+            searchModel = BaggingClassifier(DecisionTreeClassifier())
+        elif identifier == "RF":
+            searchModel = RandomForestClassifier()
+        elif identifier == "GB":
+            searchModel = GradientBoostingClassifier()
+        self.bestParams, model = self._randomizedGridSearch(searchModel, paramGrid)
+        self.validationAccuracy = accuracy_score(self.validationDataY, model.predict(self.validationDataX))
+        [self.bestParams[clauseNumber][exampleNumber]["accuracy"], self.bestParams[clauseNumber][exampleNumber]["F1Score"]] = self._calc(Y, model.predict(X))
+    
+    def test(self, identifier):
+        model = None
+        if identifier == "D":
+            model = DecisionTreeClassifier(**(self.bestParams))
+        elif identifier == "BG":
+            model = BaggingClassifier(DecisionTreeClassifier(), **(self.bestParams))
+        elif identifier == "RF":
+            model = RandomForestClassifier(**(self.bestParams))
+        elif identifier == "GB":
+            model = GradientBoostingClassifier(**(self.bestParams))
+        model.fit(self.trainX, self.trainY)
+        self.testAccuracy = accuracy_score(self.testY, model.predict(self.testX))
+    
+
+def mnistResults():
+    paramsDict = {"D": {
+            "criterion": ["gini", "entropy"],
+            "splitter": ["best", "random"],
+            "max_depth": list(range(2, 53, 2)),
+            "max_features": [None, "sqrt", "log2"],
+            "min_samples_split": list(range(2, 101, 2))
+        },
+        "BG": {
+            "n_estimators": list(range(5, 101, 1)),
+            "max_samples": np.linspace(0.1, 1.0, 10),
+            "max_features": np.linspace(0.1, 1.0, 10)
+        },
+        "RF": {
+            "n_estimators": list(range(5, 101, 1)),
+            "criterion": ["gini", "entropy"],
+            "class_weight": ["balanced", "balanced_subsample"],
+            "max_features": [None, 'sqrt', 'log2'],
+            "max_depth":list(range(2, 53, 2)),
+        },
+        "GB": {
+            "max_depth":list(range(2, 53, 2)),
+            "n_estimators": list(range(100, 1100, 100)),
+            "learning_rate": [0.1, 0.2, 0.3, 0.4, 0.5],
+            "loss": ["log_loss"],
+            "max_features": ["sqrt", "log2"]
+        }}
+    table = []
+    for identifier, paramGrid in paramsDict.items():
+        experiment = mnistExperiment()
+        experiment.search(paramGrid, identifier)
+        experiment.test(identifier)
+        row = {
+            "identifier": identifier,
+            "validationAccuracy": experiment.validationAccuracy,
+            "testAccuracy": experiment.testAccuracy
+        }
+        table.append(row)
+    
+    df = pd.DataFrame(table)
+    xlsxFile = "MNIST.xlsx"
+    df.to_excel(xlsxFile, index=False)
+    file = load_workbook(xlsxFile)
+    sheet = file.active
+    sheet.insert_rows(idx=1)
+
+    numColumns = len(df.columns)
+    sheet.merge_cells(start_row=1, start_column=1, end_row=1, end_column=numColumns)
+    sheet["A1"] = "D: decision tree classifier\nBG: bagging classifier\nRF: random forest classifier\nGB: gradient boosting classifier\nAuthor: Ian Zhang"
+    file.save(xlsxFile)
+    
+        
     
 def experiment(zipPath):
     experiments = {"D": decisionTreeExperiment(zipPath), "BG": baggingClassifierExperiment(zipPath), "RF": randomForestClassifierExperiment(zipPath), "GB": gradientBoostingClassifierExperiment(zipPath)}
@@ -229,6 +338,7 @@ def experiment(zipPath):
         mnistExperiment(key, val)
 def main():
     experiment("project2_data.zip")
+    mnistResults()
 
 
 if __name__ == "__main__":
